bf9e20f0e008510c893d99ba65637d87a1ca3055
DorneeanWebApps <office@dorneean.ro>
2022-01-25 01:16:55 +0200

    signout, update, updatenotes, delete added

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -35,14 +35,34 @@ const auth = {
   refreshToken: async (refreshToken: string): Promise<any> => {
     let response: any;
     try {
-      response = await axios.get(`${API_BASE_URL}/auth/refresh`,{
-        params: {token: refreshToken}
-      })
+      response = await axios.get(`${API_BASE_URL}/auth/refresh`, {
+        params: { token: refreshToken },
+      });
       const data = await response.data;
       return data;
     } catch (error) {
       response = error;
       console.log(error);
+    }
+    return response;
+  },
+  signOutUser: async (tokenStatus: {
+    token: string;
+    active: boolean;
+  }): Promise<any> => {
+    let response: any;
+    if (tokenStatus.active) {
+      const config = {
+        headers: { Authorization: `Bearer ${tokenStatus.token}` },
+      };
+      try {
+        response = await axios.get(`${API_BASE_URL}/auth/signout`, config);
+        const data = await response.data;
+        return data;
+      } catch (error) {
+        response = error;
+        console.log(error);
+      }
     }
     return response;
   },
