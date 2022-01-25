78c92f2ffb33095d0f35e3e7f71b9c3cdea5a9eb
DorneeanWebApps <office@dorneean.ro>
2022-01-26 00:11:33 +0200

    drop down added

--- src/components/UsersComponents/UsersTableRow.tsx
+++ src/components/UsersComponents/UsersTableRow.tsx
@@ -1,10 +1,18 @@
-import { Avatar, TableRow, TableCell, Chip, IconButton, Icon } from "@mui/material";
+import {
+  Avatar,
+  TableRow,
+  TableCell,
+  Chip,
+  IconButton,
+  Icon,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import React from "react";
 import { User } from "../../interfaces/UserInterfaces";
 import "./UsersTableRow.css";
 import { makeStyles } from "@mui/styles";
-import moment from 'moment'
-
+import moment from "moment";
 
 interface Props {
   user: User;
@@ -13,14 +21,36 @@ interface Props {
 const useStyles = makeStyles({
   tableCell: {
     color: "#6B636B",
-    display:"grid",
-    alignContent:"center",
+    display: "grid",
+    alignContent: "center",
     fontFamily: "'Montserrat',  sans-serif",
   },
 });
 
 const UsersTableRow: React.FC<Props> = ({ user }) => {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleDropDown = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleAddRoles = () => {
+    setAnchorEl(null);
+  };
+
+  const handleBlacklist = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDelete = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <TableRow
@@ -45,15 +75,54 @@ const UsersTableRow: React.FC<Props> = ({ user }) => {
         <p>{moment(user.lastLogin).format("DD.MM.YYYY")}</p>
       </TableCell>
       <TableCell align="center">
-        {user.role&&<Chip label="Activ" sx={{color:"green", borderColor:"green", width:"120px"}} variant="outlined" />}
-        {!user.role&&<Chip label="In asteptare" sx={{color:"orange", borderColor:"orange", width:"120px"}} variant="outlined" />}
+        {user.role && (
+          <Chip
+            label="Activ"
+            sx={{ color: "green", borderColor: "green", width: "120px" }}
+            variant="outlined"
+          />
+        )}
+        {!user.role && (
+          <Chip
+            label="In asteptare"
+            sx={{ color: "orange", borderColor: "orange", width: "120px" }}
+            variant="outlined"
+          />
+        )}
       </TableCell>
       <TableCell align="center">
-          <IconButton>
-              <Icon>more_vert</Icon>
-          </IconButton>
+        <IconButton
+          aria-label="more"
+          id="long-button"
+          aria-controls={open ? "long-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleDropDown}
+        >
+          <Icon>more_vert</Icon>
+        </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            "aria-labelledby": "long-button",
+          }}
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: "auto",
+              width: "130px",
+              transform: "translateX(-30px) translateY(-10px)",
+            },
+          }}
+        >
+          <MenuItem onClick={handleAddRoles}>Adauga roluri</MenuItem>
+          <MenuItem onClick={handleBlacklist}>BlackList</MenuItem>
+          <MenuItem onClick={handleDelete}>Sterge</MenuItem>
+        </Menu>
       </TableCell>
-
     </TableRow>
   );
 };
