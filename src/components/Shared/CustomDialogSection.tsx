b0924c28dc955c560bf822e4cf32d28a698c6e0e
DorneeanWebApps <office@dorneean.ro>
2022-02-01 01:09:47 +0200

    custom components commit

---
+++ src/components/Shared/CustomDialogSection.tsx
@@ -0,0 +1,23 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+interface Props {
+  title: string;
+  columnsGrid: string;
+  children: JSX.Element[] | JSX.Element;
+}
+
+const CustomDialogSection: React.FC<Props> = ({title, columnsGrid, children}) => {
+  return (
+    <Box sx={{color:"#6B636B"}}>
+      <Typography sx={{fontSize:"0.8rem", fontWeight:"700", textTransform:"uppercase", color:"#6B636B", my:"16px"}}>{title}</Typography>
+      <Box
+        sx={{ display: "grid", gridTemplateColumns: columnsGrid }}
+      >
+          {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default CustomDialogSection;
