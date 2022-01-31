7d051ad85ffe416611539d91107e9da97c986e79
DorneeanWebApps <office@dorneean.ro>
2022-02-01 01:30:02 +0200

    user table row data & refactoring

---
+++ src/interfaces/TableRowDefinitionInterface.ts
@@ -0,0 +1,21 @@
+export interface MenuItemAction{
+    action:string; 
+    text:string
+};
+
+export interface TextField{
+    name: string
+    weight?:"bold"|"regular"|undefined
+}
+
+export interface RowCell {
+    cellType: string;
+    altField?: string|undefined;
+    align?: "center"|"justify"|"left"|"right"|"inherit";
+    field?: string;
+    fields?: TextField[];
+    dateFormat?: string|undefined;
+    ifExistsText?: string|undefined;
+    ifNotExistsText?: string|undefined;
+    menuItems?: MenuItemAction[]|undefined
+}
