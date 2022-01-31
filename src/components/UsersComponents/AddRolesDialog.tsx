36ad558af6b74f4a77f86d22936b5dc1747b1ac1
DorneeanWebApps <office@dorneean.ro>
2022-02-01 01:34:24 +0200

    add users filters

---
+++ src/components/UsersComponents/AddRolesDialog.tsx
@@ -0,0 +1,70 @@
+import { Checkbox, FormControlLabel } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import CustomDialog from "../Shared/CustomDialog";
+import CustomDialogSection from "../Shared/CustomDialogSection";
+import userService from "../../services/userService";
+import useTokenStatus from "../../utils/useTokenStatus";
+
+interface Props {
+  open: boolean;
+  onClose: any;
+  id: string | number | undefined;
+  onRoles: any;
+}
+
+const AddRolesDialog: React.FC<Props> = ({ open, onClose, id, onRoles }) => {
+  const [roles, setRoles] = useState<number[]>([]);
+  const tokenStatus = useTokenStatus();
+
+  useEffect(() => {
+    if (open) {
+    }
+  }, [open]);
+
+  const handleActions = () => {};
+
+  const handleRoles = () => {};
+  return (
+    <CustomDialog
+      title="Modifica roluri urilizator"
+      actionText="modifica"
+      icon="tune"
+      open={open}
+      onClose={onClose}
+      onAction={handleActions}
+    >
+      <CustomDialogSection title="selecteaza roluri" columnsGrid="1fr 1fr 1fr">
+        <FormControlLabel
+          value={250}
+          checked={roles.includes(250)}
+          onChange={handleRoles}
+          control={<Checkbox />}
+          label="admin"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={150}
+          checked={roles.includes(150)}
+          onChange={handleRoles}
+          control={<Checkbox />}
+          label="coordinator"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={70}
+          onChange={handleRoles}
+          checked={roles.includes(70)}
+          control={<Checkbox />}
+          label="reviewer"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={10}
+          checked={roles.includes(10)}
+          onChange={handleRoles}
+          control={<Checkbox />}
+          label="researcher"
+        ></FormControlLabel>
+      </CustomDialogSection>
+    </CustomDialog>
+  );
+};
+
+export default AddRolesDialog;
--- src/components/UsersComponents/UsersFiltersDialog.tsx
+++ src/components/UsersComponents/UsersFiltersDialog.tsx
@@ -1,21 +1,19 @@
 import React, { useEffect, useState } from "react";
 import {
-  Dialog,
-  DialogContent,
-  DialogActions,
-  Button,
   Checkbox,
   FormControlLabel,
   FormControl,
   InputLabel,
   Select,
   MenuItem,
+  Radio,
 } from "@mui/material";
 
-import "./UsersFiltersDialog.css";
 
 import { Filters } from "../../interfaces/UserInterfaces";
-import CustomDialogHeader from "../Shared/CustomDialogHeader";
+import CustomDialogSection from "../Shared/CustomDialogSection";
+import { Box } from "@mui/system";
+import CustomDialog from "../Shared/CustomDialog";
 
 interface Props {
   open: boolean;
@@ -24,140 +22,163 @@ interface Props {
   onFilters: any;
 }
 
-
-const UsersFiltersDialog: React.FC<Props> = ({ open, onClose, onFilters, filters }) => {
-
+const UsersFiltersDialog: React.FC<Props> = ({
+  open,
+  onClose,
+  onFilters,
+  filters,
+}) => {
   const [selectedFilters, setFilters] = useState<Filters>(filters);
-  const [wereLogged, setWereLogged] =useState<number|null>(null)
+  const [wereLogged, setWereLogged] = useState<number | null>(null);
 
-  const handleStatuses = (e:any) =>{
-    const statuses =  selectedFilters.statusFilters.includes(parseInt(e.target.value)) ? selectedFilters.statusFilters.filter(item=>item!==parseInt(e.target.value)) : [...selectedFilters.statusFilters, parseInt(e.target.value)];
-    setFilters({...selectedFilters, statusFilters: [...statuses]});
-  }
+  const handleStatuses = (e: any) => {
+    const statuses = selectedFilters.statusFilters.includes(
+      parseInt(e.target.value)
+    )
+      ? selectedFilters.statusFilters.filter(
+          (item) => item !== parseInt(e.target.value)
+        )
+      : [...selectedFilters.statusFilters, parseInt(e.target.value)];
+    setFilters({ ...selectedFilters, statusFilters: [...statuses] });
+  };
 
-  const handleRoles = (e:any) =>{
-    const roles = selectedFilters.roleFilters.includes(parseInt(e.target.value)) ? selectedFilters.roleFilters.filter(item=>item!==parseInt(e.target.value)) : [...selectedFilters.roleFilters, parseInt(e.target.value)];
-    setFilters({...selectedFilters, roleFilters: [...roles]});
-  }
+  const handleRoles = (e: any) => {
+    const roles = selectedFilters.roleFilters.includes(parseInt(e.target.value))
+      ? selectedFilters.roleFilters.filter(
+          (item) => item !== parseInt(e.target.value)
+        )
+      : [...selectedFilters.roleFilters, parseInt(e.target.value)];
+    setFilters({ ...selectedFilters, roleFilters: [...roles] });
+  };
 
-  const handleWereLogged = (e:any) =>{
+  const handleWereLogged = (e: any) => {
     setWereLogged(parseInt(e.target.value));
-    setFilters({...selectedFilters, lastDateFilter:{logged:parseInt(e.target.value), period: selectedFilters.lastDateFilter.period}});
-  }
+    setFilters({
+      ...selectedFilters,
+      lastDateFilter: {
+        logged: parseInt(e.target.value),
+        period: selectedFilters.lastDateFilter.period,
+      },
+    });
+  };
 
-  const handlePeriod = (e:any) => {
+  const handlePeriod = (e: any) => {
     const loggedSelected = selectedFilters.lastDateFilter.logged;
     setWereLogged(!loggedSelected ? 0 : loggedSelected);
-    setFilters({...selectedFilters, lastDateFilter:{logged: !loggedSelected ? 0 : loggedSelected,  period:parseInt(e.target.value)}});
-  }
+    setFilters({
+      ...selectedFilters,
+      lastDateFilter: {
+        logged: !loggedSelected ? 0 : loggedSelected,
+        period: parseInt(e.target.value),
+      },
+    });
+  };
 
-  const handleClose = () =>{
+  const handleActions = () => {
     onFilters(selectedFilters);
-  }
+  };
 
-  useEffect(()=>{
-    setFilters(filters)
-  },[filters])
+  useEffect(() => {
+    setFilters(filters);
+  }, [filters]);
 
   return (
-    <Dialog
-      PaperProps={{
-        sx: {
-          width: "600px",
-        },
-      }}
-      onClose={onClose}
+    <CustomDialog
+      title="Filtreaza utilizatori"
+      actionText="filtreaza"
+      icon="tune"
       open={open}
+      onClose={onClose}
+      onAction={handleActions}
     >
-      <CustomDialogHeader title="Filtreaza utilizatori" icon="tune" onClose={onClose}></CustomDialogHeader>
-      <DialogContent>
-        <div className="filters-section">
-          <h5>filtreaza utilizatori dupa status</h5>
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
-            <FormControlLabel
-              value = {0}
-              onChange={handleStatuses}
-              checked={selectedFilters.statusFilters.includes(0)}
-              control={<Checkbox />}
-              label="in asteptare"
-            ></FormControlLabel>
-            <FormControlLabel
-              value = {1}
-              checked={selectedFilters.statusFilters.includes(1)}
-              onChange={handleStatuses}
-              control={<Checkbox />}
-              label="activi"
-            ></FormControlLabel>
-          </div>
-        </div>
-        <div className="filters-section">
-          <h5>filtreaza utilizatori dupa rol</h5>
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
-            <FormControlLabel
-              value = {250}
-              checked={selectedFilters.roleFilters.includes(250)}
-              onChange={handleRoles}
-              control={<Checkbox />}
-              label="admin"
-            ></FormControlLabel>
-            <FormControlLabel
-              value = {150}
-              checked={selectedFilters.roleFilters.includes(150)}
-              onChange={handleRoles}
-              control={<Checkbox />}
-              label="coordinator"
-            ></FormControlLabel>
-            <FormControlLabel
-              value = {70}
-              onChange={handleRoles}
-              checked={selectedFilters.roleFilters.includes(70)}
-              control={<Checkbox />}
-              label="reviewer"
-            ></FormControlLabel>
-            <FormControlLabel
-              value = {10}
-              checked={selectedFilters.roleFilters.includes(10)}
-              onChange={handleRoles}
-              control={<Checkbox />}
-              label="researcher"
-            ></FormControlLabel>
-          </div>
-        </div>
-        <div className="filters-section">
-          <h5>filtreaza utilizatori dupa ultima logare</h5>
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-            <div className="logged-status-group">
-              <FormControlLabel
-                control={<Checkbox />}
-                label="s-au logat"
-                value = {1}
-                checked = { wereLogged === 1}
-                onChange={handleWereLogged}
-              ></FormControlLabel>
-              <FormControlLabel
-                control={<Checkbox />}
-                label="nu s-au logat"
-                value = {0}
-                checked = { wereLogged === 0 }
-                onChange={handleWereLogged}
-              ></FormControlLabel>
-            </div>
+      <CustomDialogSection
+        title="filtreaza utilizatori dupa status"
+        columnsGrid="1fr 1fr 1fr"
+      >
+        <FormControlLabel
+          value={0}
+          onChange={handleStatuses}
+          checked={selectedFilters.statusFilters.includes(0)}
+          control={<Checkbox />}
+          label="in asteptare"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={1}
+          checked={selectedFilters.statusFilters.includes(1)}
+          onChange={handleStatuses}
+          control={<Checkbox />}
+          label="activi"
+        ></FormControlLabel>
+      </CustomDialogSection>
+      <CustomDialogSection
+        title="filtreaza utilizatori dupa rol"
+        columnsGrid="1fr 1fr 1fr"
+      >
+        <FormControlLabel
+          value={250}
+          checked={selectedFilters.roleFilters.includes(250)}
+          onChange={handleRoles}
+          control={<Checkbox />}
+          label="admin"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={150}
+          checked={selectedFilters.roleFilters.includes(150)}
+          onChange={handleRoles}
+          control={<Checkbox />}
+          label="coordinator"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={70}
+          onChange={handleRoles}
+          checked={selectedFilters.roleFilters.includes(70)}
+          control={<Checkbox />}
+          label="reviewer"
+        ></FormControlLabel>
+        <FormControlLabel
+          value={10}
+          checked={selectedFilters.roleFilters.includes(10)}
+          onChange={handleRoles}
+          control={<Checkbox />}
+          label="researcher"
+        ></FormControlLabel>
+      </CustomDialogSection>
 
-            <FormControl fullWidth>
-              <InputLabel>alege perioada</InputLabel>
-              <Select value={selectedFilters.lastDateFilter.period} label="alege perioada" onChange={handlePeriod}>
-                <MenuItem value={7}>ultima saptamana</MenuItem>
-                <MenuItem value={30}>ultima luna</MenuItem>
-                <MenuItem value={90}>ultimile 3 luni</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-        </div>
-      </DialogContent>
-      <DialogActions sx={{padding: "16px", borderTop: "1px solid #bdbdbd"}}>
-        <Button onClick={()=>handleClose()}>Filtreaza</Button>
-      </DialogActions>
-    </Dialog>
+      <CustomDialogSection
+        title="filtreaza utilizatori dupa ultima logare"
+        columnsGrid="1fr 1fr"
+      >
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <FormControlLabel
+            control={<Radio />}
+            label="s-au logat"
+            value={1}
+            checked={wereLogged === 1}
+            onChange={handleWereLogged}
+          ></FormControlLabel>
+          <FormControlLabel
+            control={<Radio />}
+            label="nu s-au logat"
+            value={0}
+            checked={wereLogged === 0}
+            onChange={handleWereLogged}
+          ></FormControlLabel>
+        </Box>
+
+        <FormControl fullWidth>
+          <InputLabel>alege perioada</InputLabel>
+          <Select
+            value={selectedFilters.lastDateFilter.period}
+            label="alege perioada"
+            onChange={handlePeriod}
+          >
+            <MenuItem value={7}>ultima saptamana</MenuItem>
+            <MenuItem value={30}>ultima luna</MenuItem>
+            <MenuItem value={90}>ultimile 3 luni</MenuItem>
+          </Select>
+        </FormControl>
+      </CustomDialogSection>
+    </CustomDialog>
   );
 };
 
