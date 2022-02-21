9bfd87a2fbda19a8fc77dc32eb10287e329b65dc
mihaipmp <mihaipmp@gmail.com>
2022-02-21 17:52:01 +0200

    working on the translation

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,12 +2,16 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import translation from "./locales/en/translation.json";
+import translationEN from "./locales/en/translation.json";
+import translationRO from "./locales/ro/translation.json";
 
 export const defaultNS = "translation";
 export const resources = {
   en: {
-    translation,
+    translationEN,
+  },
+  ro: {
+    translationRO,
   },
 } as const;
 
@@ -17,22 +21,25 @@ i18n
   .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
+  //.use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en",
+    fallbackLng: "ro",
     debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    lng: "en",
+    lng: "ro",
     ns: ["translation"],
     defaultNS,
     resources,
+    react: {
+      useSuspense: false,
+    },
   });
 
 export default i18n;
