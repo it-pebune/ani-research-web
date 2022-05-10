1c2d1ca0c106e31b9f19b33dab51b287af9dee46
Andrei Croitoru-Rusan <a.croitoru3@icloud.com>
2022-05-10 07:23:04 +0200

    Convert ReactDOM.render() to render() with createRoot()
    As per https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,7 +8,10 @@ import { AuthContextProvider } from "./store/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import { UserContextProvider } from "./store/UserContext";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <Router>
       <AuthContextProvider>
@@ -19,8 +22,7 @@ ReactDOM.render(
         </UserContextProvider>
       </AuthContextProvider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
