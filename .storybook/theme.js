da7f938b8ddb370f48a2e48d456edf21384bb484
Andrei Croitoru-Rusan <a.croitoru3@icloud.com>
2022-05-25 02:54:42 +0200

    Update .storybook/theme.js

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,7 +2,7 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "dark",
-  brandTitle: "My custom storybook",
+  brandTitle: "IT-PeBune design system",
   brandUrl: "https://github.com/it-pebune",
   brandImage:
     "https://integritatepebune.ro/_next/image?url=%2Fimages%2Fprofile.png&w=384&q=75",
