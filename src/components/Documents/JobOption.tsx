fba4b18468e5d9a3f69f379e49756be409394800
Ionut Breta <breta.ionut@gmail.com>
2022-07-31 01:00:43 +0300

    - added menu for job actions

--- src/components/Documents/JobOption.tsx
+++ src/components/Documents/JobOption.tsx
@@ -1,7 +1,16 @@
 import { Job } from "../../interfaces/JobInterfaces";
-import React, { MouseEventHandler, ReactElement } from "react";
-import { Box, Radio, Typography } from "@mui/material";
+import React, { MouseEventHandler, ReactElement, useState } from "react";
+import {
+  Box,
+  Icon,
+  IconButton,
+  Menu,
+  MenuItem,
+  Radio,
+  Typography,
+} from "@mui/material";
 import moment from "moment";
+import { MouseEvent } from "react";
 
 interface Props {
   job: Job;
@@ -14,11 +23,19 @@ export const JobOption: React.FC<Props> = ({
   checked,
   handleRadioClick,
 }: Props): ReactElement => {
+  const [menuAnchorElement, setMenuAnchorElement] = useState<Element | null>(
+    null
+  );
+
   const uppercaseFirstLetter = (word: string): string =>
       word.charAt(0).toUpperCase() + word.slice(1),
     uppercaseFirstLetterOfEachWord = (words: string): string =>
       words.toLowerCase().split(" ").map(uppercaseFirstLetter).join(" "),
     formatDate = (date: string): string => moment(date).format("MM.YYYY");
+
+  const openMenu = (event: MouseEvent): void =>
+      setMenuAnchorElement(event.currentTarget),
+    closeMenu = (): void => setMenuAnchorElement(null);
 
   return (
     <Box
@@ -33,7 +50,38 @@ export const JobOption: React.FC<Props> = ({
 
       <Box>
         <Typography variant="body1">
-          <b>{job.name}</b>
+          <b>{job.name}</b>{" "}
+          <IconButton
+            id={`long-button-${job.id}`}
+            aria-label="more"
+            aria-controls={!!menuAnchorElement ? "long-menu" : undefined}
+            aria-expanded={!!menuAnchorElement ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={openMenu}
+          >
+            <Icon>more_horizontal</Icon>
+          </IconButton>
+          <Menu
+            id={`long-menu-${job.id}`}
+            anchorEl={menuAnchorElement}
+            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+            open={!!menuAnchorElement}
+            onClose={closeMenu}
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+            }}
+            PaperProps={{
+              style: {
+                maxHeight: "auto",
+                width: "100px",
+                transform: "translateY(-10px)",
+              },
+            }}
+          >
+            <MenuItem key="edit">Editeaza</MenuItem>
+
+            <MenuItem key="delete">Sterge</MenuItem>
+          </Menu>
         </Typography>
 
         <Typography variant="body2">
