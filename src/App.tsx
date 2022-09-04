20d22dd1017aeb3a8438f10f1cc8cd4edda153f0
Ionut Breta <breta.ionut@gmail.com>
2022-09-05 00:08:24 +0300

    - added the assigned subject documents page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import AssignedSubjects from "./pages/DashboardPage/AssignedSubjects";
 import ReviewPdf from "./pages/DashboardPage/ReviewPdf";
 import UserProfilePage from "./pages/UserProfilePage/UserProfilePage";
 import { DocumentsFromScrapper } from "./pages/DashboardPage/DocumentsFromScrapper";
+import { AssignedSubjectDocuments } from "./pages/DashboardPage/AssignedSubjectDocuments";
 
 const App: React.FC = () => {
   return (
@@ -36,6 +37,10 @@ const App: React.FC = () => {
               <Route path="/subjects" element={<Subjects />} />
               <Route path="/assigned-subjects" element={<AssignedSubjects />} />
               <Route path="/review-pdf/:id" element={<ReviewPdf />} />
+              <Route
+                path="/documents/:subjectId"
+                element={<AssignedSubjectDocuments />}
+              />
               <Route
                 path="/scrapped-docs/:subjectId"
                 element={<DocumentsFromScrapper />}
