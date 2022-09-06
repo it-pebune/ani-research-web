057c62fdf6e66ee3c1a1e9f1b8be09c6dba77502
Ionut Breta <breta.ionut@gmail.com>
2022-09-07 01:19:28 +0300

    - added logic for loading the documents in the assigned subject documents page

--- src/pages/DashboardPage/AssignedSubjectDocuments.tsx
+++ src/pages/DashboardPage/AssignedSubjectDocuments.tsx
@@ -1,5 +1,53 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import SearchBarWithFiltersController from "../../components/Shared/SearchBarWithFiltersController";
+import { useParams } from "react-router";
+import useTokenStatus from "../../utils/useTokenStatus";
+import { DocumentFromDataBase } from "../../interfaces/DocumentInterfaces";
+import { documentService } from "../../services/documentsServices";
+import Loader from "../../components/Shared/Loader";
 
 export const AssignedSubjectDocuments: React.FC = (): ReactElement => {
-  return <></>;
+  const tokenStatus = useTokenStatus(),
+    subjectId = useParams().subjectId as unknown as number,
+    [loading, setLoading] = useState<boolean>(false),
+    [documents, setDocuments] = useState<DocumentFromDataBase[]>([]),
+    [visibleDocuments, setVisibleDocuments] = useState<DocumentFromDataBase[]>(
+      []
+    );
+
+  useEffect((): void => {
+    const loadDocuments = async (): Promise<void> => {
+      setLoading(true);
+
+      const loadedDocuments = await documentService.getDocumentsFromDataBase({
+        ...tokenStatus,
+        subjectId,
+      });
+
+      setLoading(false);
+
+      setDocuments(loadedDocuments);
+      setVisibleDocuments(loadedDocuments);
+    };
+
+    loadDocuments();
+  }, [subjectId]);
+
+  return (
+    <>
+      {loading && <Loader />}
+
+      {!loading && (
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box sx={{ p: "16px" }}>
+            <SearchBarWithFiltersController
+              onFiltersOpen={() => {}}
+              onSearchChanged={() => {}}
+            />
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 };
